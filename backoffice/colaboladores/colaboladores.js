410c9cfc48bb7b2ae3caca5fca386c7bc54e5d04
RomelCardoso <romelcardosojesus@gmail.com>
2023-07-24 19:28:33 -0300

    atualizar

--- backoffice/colaboladores/colaboladores.js
+++ backoffice/colaboladores/colaboladores.js
@@ -1,10 +1,24 @@
 const dadosgrid = document.querySelector("#dadosgrid");
+const btn_add = document.querySelector("#btn_add");
+const novoColaborador = document.querySelector("#novoColaborador");
+const btn_gravarPop = document.querySelector("#btn_gravarPop");
+const btn_cancelarPop = document.querySelector("#btn_cancelarPop");
+const telefones = document.querySelector("#telefones");
+const f_fone = document.querySelector("#f_fone");
+const f_nome = document.querySelector("#f_nome");
+const f_tipoColab = document.querySelector("#f_tipoColab");
+const f_status = document.querySelector("#f_status");
+const f_foto= document.querySelector("#f_foto");
+const img_foto= document.querySelector("#img_foto");
+
+
+// comandos
 const endpoint_todoscolaboradores = `http://127.0.0.1:1880/todosUsuarios`;
 fetch(endpoint_todoscolaboradores)
     .then(res => res.json())
     .then(res => {
         console.log(res);
-        dadosgrid.innerHTML="";
+        dadosgrid.innerHTML = "";
         res.forEach(e => {
             const divlinha = document.createElement("div");
             divlinha.setAttribute("class", "linhaGrid");
@@ -43,4 +57,132 @@ fetch(endpoint_todoscolaboradores)
 
         });
 
+    });
+
+const endpoint_tiposColab = `http://127.0.0.1:1880/tiposColab`;
+fetch(endpoint_tiposColab)
+    .then(res => res.json())
+    .then(res => {
+        f_tipoColab.innerHTML = "";
+        res.forEach(e => {
+            const opt = document.createElement("option");
+
+            opt.setAttribute("value", e.n_tipousuario_tipousuario);
+            opt.innerHTML = e.s_desc_tipousuario;
+            f_tipoColab.appendChild(opt);
+
+        });
     })
+
+btn_add.addEventListener("click", (evt) => {
+    novoColaborador.classList.remove("ocultarPopup");
+
+});
+
+btn_fecharPopup.addEventListener("click", (evt) => {
+    novoColaborador.classList.add("ocultarPopup");
+
+});
+
+btn_gravarPop.addEventListener("click", (evt) => {
+
+    // novoColaborador.classList.add("ocultarPopup");
+    const tels=[...document.querySelectorAll(".Numtel")];
+    let numTels=[];
+    tels.forEach(t=>{
+        numTels.push(t.innerHTML);
+
+    });
+
+    const dados={
+        s_nome_usuario:f_nome.value,
+        n_tipousuario_tipousuario:f_tipoColab.value,
+        c_status_usuario:f_status.value,
+        numtelefones:numTels,
+        s_foto_usuario:img_foto.getAttribute("src")
+
+
+    }
+
+    const cab={
+        method:'POST',
+        body:JSON.stringify(dados) 
+    }
+    const endpointnovocolab=`http://127.0.0.1:1880/novocolab`
+    fetch(endpointnovocolab,cab)
+    .then(res=>{
+        if(res.status==200){
+
+            alert("Novo colaborador gravado");
+            f_nome.value="";
+            f_tipoColab.value="";
+            f_status.value="";
+            f_foto.value="";
+            img_foto.setAttribute("src","#");
+            telefones.innerHTML="";
+          
+
+            
+        }else{
+            alert("Erro ao grava Novo colaborador");
+            
+        }
+    })
+    
+});
+
+btn_cancelarPop.addEventListener("click", (evt) => {
+    novoColaborador.classList.add("ocultarPopup");
+});
+
+f_fone.addEventListener("keyup", (evt) => {
+    if (evt.key == "Enter") {
+        if(evt.target.value.length >= 8){
+               const divTel=document.createElement("div");
+        divTel.setAttribute("class","tel");
+
+        const Numtel=document.createElement("div");
+        Numtel.setAttribute("class","Numtel");
+        Numtel.innerHTML=evt.target.value;
+        divTel.appendChild(Numtel);
+
+        const delTel=document.createElement("img");
+        delTel.setAttribute("src","../../imgs/delete.svg");
+        delTel.setAttribute("class","delTel");
+        delTel.addEventListener("click",(evt)=>{
+            evt.target.parentNode.remove();
+        });
+        divTel.appendChild(delTel);
+
+        telefones.appendChild(divTel);
+
+        evt.target.value="";
+        }else{
+            alert("Número de telefone inválido!");
+        }
+        // console.log(evt.target.value);
+     
+
+
+    }
+
+});
+
+const converter_imagem_b64=(localDestino,arquivoimg)=>{
+    const obj=arquivoimg;
+    const reader=new FileReader();
+    reader.addEventListener("load",(evt)=>{
+        localDestino.src=reader.result;
+
+    });
+    if(obj){
+        reader.readAsDataURL(obj);
+    }
+}
+
+f_foto.addEventListener("change", (evt)=>{
+    converter_imagem_b64(img_foto,evt.target.files[0]);
+
+
+});
+
